9f165d4010d344b1d0dcacf8e9e9cb945106f4b4
AbbMarPin <54662893+AbbMarPin@users.noreply.github.com>
2020-04-04 18:47:55 +0200

    init

---
+++ vuenotif/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-text-field
+      name="url"
+      label="url"
+      v-model="url"
+    ></v-text-field>
+    <v-form>
+      <v-text-field
+        name="Titel"
+        label="Titel"
+        v-model="Titel"
+      ></v-text-field>
+      <v-text-field
+        name="message"
+        label="message"
+        v-model="message"
+      ></v-text-field>
+      <v-btn color="success" @click="send()">send</v-btn>
+    </v-form>
+    <v-alert type="primary" :value="response">
+      {{response}}
+    </v-alert>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+const axios = require('axios');
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      Titel: "",
+      message: "",
+      url: "localhost",
+      response: ""
+
+    }
+  },
+  methods: {
+    send(){
+      let data = {
+        "title": this.title,
+        "message": this.message
+      }
+      let self = this;
+      axios.get(this.url + ":8000", data).then(function (response) {
+        self.response = response.data
+      })
+    }
+  },
+}
+</script>
