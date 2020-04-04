0f695814d37c9d433478c49fb5155cf7a4fcd1f3
AbbMarPin <54662893+AbbMarPin@users.noreply.github.com>
2020-04-04 19:07:15 +0200

    done

--- vuenotif/src/views/Home.vue
+++ vuenotif/src/views/Home.vue
@@ -1,55 +1,51 @@
 <template>
   <v-app>
-    <v-text-field
-      name="url"
-      label="url"
-      v-model="url"
-    ></v-text-field>
-    <v-form>
-      <v-text-field
-        name="Titel"
-        label="Titel"
-        v-model="Titel"
-      ></v-text-field>
-      <v-text-field
-        name="message"
-        label="message"
-        v-model="message"
-      ></v-text-field>
-      <v-btn color="success" @click="send()">send</v-btn>
-    </v-form>
-    <v-alert type="primary" :value="response">
-      {{response}}
-    </v-alert>
+    <v-card class="ma-9">
+      <v-text-field name="url" label="url" v-model="url"></v-text-field>
+      <v-form>
+        <v-text-field name="title" label="title" v-model="title"></v-text-field>
+        <v-text-field name="message" label="message" v-model="message"></v-text-field>
+        <v-text-field
+          name="img"
+          label="img"
+          v-model="img"
+        ></v-text-field>
+        <v-btn class="ma-4" color="success" @click="send()">send</v-btn>
+      </v-form>
+      <v-alert :value="Boolean(response)">{{response}}</v-alert>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-const axios = require('axios');
+const axios = require("axios");
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      Titel: "",
+      title: "",
       message: "",
-      url: "localhost",
+      img: "",
+      url: "http://192.168.0.54",
       response: ""
-
-    }
+    };
   },
   methods: {
-    send(){
+    send() {
       let data = {
-        "title": this.title,
-        "message": this.message
-      }
+        title: this.title,
+        message: this.message,
+        imgurl: this.img
+      };
+      console.log(data)
       let self = this;
-      axios.get(this.url + ":8000", data).then(function (response) {
-        self.response = response.data
-      })
+      let url = this.url + ":8000";
+      axios.post(url, data).then(function(response) {
+        self.response = response.data;
+      });
     }
-  },
-}
+  }
+};
 </script>
